8c6ffcf03a7cb78d33341dff5a9c0e6dbf64ea83
Gaurav Singh <gaurav.s.140102@gmail.com>
2021-10-10 22:28:31 +0530

    Configure signup and login functionality, add password hashing and token creation

--- app.js
+++ app.js
@@ -1,72 +1,54 @@
-const express=require('express');
-const mongoose = require('mongoose');
-const app=express();
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const { cloudinary } = require("./utils/cloudinary");
 
-const { cloudinary } = require('./utils/cloudinary');
-
-
-require('./db/conn');
+require("./db/conn");
 
 app.use(express.json());
 
+app.use(require("./router/auth"));
 
-app.use(require('./router/auth'));
+// app.get("/", (req, res) => {
+// 	res.send("HELLO THERE");
+// });
 
+// const middleware = (req, res, next) => {
+// 	console.log("hello middle");
+// 	next();
+// };
 
-
-
-
-app.get('/', (req,res)=> {
-    res.send('HELLO THERE');
-
+app.get("/createv", (req, res) => {
+	res.send("CREATE EVENT");
 });
 
-const middleware =(req, res, next) =>
-{
-    console.log('hello middle')
-    next()
-}
-
-app.get('/createv', (req,res)=> {
-    res.send('CREATE EVENT');
-
+app.get("/event", (req, res) => {
+	res.send("EVENTS:");
 });
 
-app.get('/event', (req,res) => {
-    res.send('EVENTS:');
-})
-
-app.get('/about' ,middleware, (req,res)=> {
-    res.send('DARTH SIDIOUS AND LORD VADER');
-
-});
-  
-app.listen(2000,()=>{
-    console.log('server is running');
+app.listen(2000, () => {
+	console.log("server is running");
 });
 
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.static('public'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+let imageurl = "";
 
-
-let imageurl="";
-
-app.post('/image', async (req, res) => {
-    try {
-        const fileStr = req.body.data;
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-            upload_preset: 'eventimages',
-        });
-        imageurl =uploadResponse.url;
-        res.send(imageurl);
-        console.log(imageurl);
-        
-    } catch (err) {
-        console.error(err);
-    }
-    });
-app.get('/image', (req, res) => res.send({imageurl}));
-
-
+app.post("/image", async (req, res) => {
+	try {
+		const fileStr = req.body.data;
+		const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+			upload_preset: "eventimages",
+		});
+		imageurl = uploadResponse.url;
+		res.send(imageurl);
+		console.log(imageurl);
+	} catch (err) {
+		console.error(err);
+	}
+});
+app.get("/image", (req, res) => res.send({ imageurl }));
--- router/auth.js
+++ router/auth.js
@@ -1,103 +1,164 @@
-const express=require('express');
-const router=express.Router();
-var MongoClient=require('mongodb').MongoClient;
-const app= express();
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+var MongoClient = require("mongodb").MongoClient;
+const authenticate = require("../middleware/authenticate");
+const app = express();
 
-
-const db='mongodb+srv://eventgalore:eventgalore@cluster0.kz4zp.mongodb.net/events_galore?retryWrites=true&w=majority'
-const  Event=require('../model/eventSchema');
-
-
-router.get('/', (req,res)=> {
-    res.send('HELLO THERE from auth');
-
+const db =
+	"mongodb+srv://eventgalore:eventgalore@cluster0.kz4zp.mongodb.net/events_galore?retryWrites=true&w=majority";
+const Event = require("../model/eventSchema");
+const User = require("../model/userSchema");
+router.get("/", (req, res) => {
+	res.send("HELLO THERE from auth");
 });
 
+router.post("/createv", authenticate, async (req, res) => {
+	const {
+		title,
+		description,
+		image,
+		cost,
+		venue,
+		eventspeaker,
+		contact,
+		tags,
+		ispaid,
+		isoffline,
+		link,
+		isfeatured,
+		date,
+		time,
+		no_of_users,
+	} = req.body;
 
+	if (!title) {
+		return res.status(422).json({ error: "fill the title" });
+	}
+	if (!description) {
+		return res.status(422).json({ error: "fill the description" });
+	}
+	if (!eventspeaker) {
+		return res.status(422).json({ error: "fill the fields" });
+	}
+	if (!contact) {
+		return res.status(422).json({ error: "fill the contact" });
+	}
+	if (!tags) {
+		console.log("enter tag");
+	}
+	if (!ispaid) {
+		console.log("enter paid");
+	}
+	if (!isoffline) {
+		console.log("enter offline");
+	}
+	if (!isfeatured) {
+		console.log("enter featured");
+	}
+	if (!date) {
+		console.log("enter date");
+	}
+	if (!time) {
+		console.log("enter time");
+	}
 
-
-router.post('/createv', async (req,res)=>{
-    const  { title, description, image, cost, venue, eventspeaker, contact, tags, ispaid, isoffline, link, isfeatured, date, time, no_of_users}=req.body;
-  
-    if( !title)
-    {
-        return res.status(422).json({ error:"fill the title"});
-    }
-    if( !description)
-    {
-        return res.status(422).json({ error:"fill the description"});
-    }
-    if( !eventspeaker)
-    {
-        return res.status(422).json({ error:"fill the fields"});
-    }
-    if( !contact)
-    {
-        return res.status(422).json({ error:"fill the contact"});
-    }
-    if( !tags)
-    {
-        console.log("enter tag")
-    }
-    if( !ispaid)
-    {
-        console.log("enter paid")
-    }
-    if( !isoffline)
-    {
-        console.log("enter offline");
-    }
-    if( !isfeatured)
-    {
-        console.log('enter featured');
-    }
-    if( !date)
-    {
-        console.log("enter date");
-    }
-    if( !time)
-    {
-        console.log('enter time');
-    }
-    
-    
-       
-    const cevent=new Event({ title, description, image, cost, venue, eventspeaker, contact, tags, ispaid, isoffline, isfeatured,link, date, time, no_of_users});
-    cevent.save().then(() =>
-    {
-        console.log('ZA WARUDO')
-    }).catch((err) =>
-       console.log(err));
-    
-    
+	const cevent = new Event({
+		title,
+		description,
+		image,
+		cost,
+		venue,
+		eventspeaker,
+		contact,
+		tags,
+		ispaid,
+		isoffline,
+		isfeatured,
+		link,
+		date,
+		time,
+		no_of_users,
+	});
+	cevent
+		.save()
+		.then(() => {
+			console.log("ZA WARUDO");
+		})
+		.catch((err) => console.log(err));
 });
 
-router.get('/event', (req,res)=> {
-    
-    MongoClient.connect(db, function(err, client) {
-        var db=client.db('events_galore');
-        if (err) throw err;
-        db.collection("events").find({isfeatured:"yes"}).toArray((err, result)=> {
-        if (err) throw err;
-        res.send(result)
-        });
-    });
+router.get("/event", (req, res) => {
+	MongoClient.connect(db, function (err, client) {
+		var db = client.db("events_galore");
+		if (err) throw err;
+		db.collection("events")
+			.find({ isfeatured: "yes" })
+			.toArray((err, result) => {
+				if (err) throw err;
+				res.send(result);
+			});
+	});
 });
 
-
-
-router.get('/date', (req,res)=> {
-    
-    MongoClient.connect(db, function(err, client) {
-        var db=client.db('events_galore');
-        if (err) throw err;
-        db.collection("events").find({isfeatured:"yes"}).sort({'id':1}).toArray((err, result)=> {
-        if (err) throw err;
-        res.send(result)
-        });
-    });
+router.get("/date", (req, res) => {
+	MongoClient.connect(db, function (err, client) {
+		var db = client.db("events_galore");
+		if (err) throw err;
+		db.collection("events")
+			.find({ isfeatured: "yes" })
+			.sort({ id: 1 })
+			.toArray((err, result) => {
+				if (err) throw err;
+				res.send(result);
+			});
+	});
 });
 
+router.post("/register", async (req, res) => {
+	const { firstname, lastname, username, email, password } = req.body;
+	try {
+		const userExists = await User.findOne({ email: email });
+		if (userExists) {
+			throw "Invalid Registration";
+		} else {
+			const user = new User({ firstname, lastname, username, email, password });
+			await user.save();
+			res.status(201).json({ message: "User registered successfully" });
+		}
+	} catch (err) {
+		return res.status(422).json({ error: `${err}` });
+	}
+});
 
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		let token;
+		const userFound = await User.findOne({ email: email });
+		if (userFound) {
+			const verifyPassword = await bcrypt.compare(password, userFound.password);
+			token = await userFound.generateAuthToken();
+			// console.log(token);
 
-module.exports =router;
+			res.cookie("jwtoken", token, {
+				expires: new Date(Date.now() + 86400000),
+				httpOnly: true,
+			});
+
+			if (verifyPassword) {
+				return res.status(200).json({ Success: "Login Successful" });
+			} else {
+				throw "Invalid Credentials";
+			}
+		} else {
+			// return res.status(400).json({ error: "Invalid Credentials" });
+			throw "Invalid Credentials";
+		}
+	} catch (err) {
+		return res.status(400).json({ error: `${err}` });
+	}
+});
+
+module.exports = router;
