7643fb40c7cb0b0fbbb064d0458bf8f87c918a86
Gaurav Singh <gaurav.s.140102@gmail.com>
2023-07-16 19:10:34 +0530

    Reset headers to allow CORS

--- app.js
+++ app.js
@@ -15,10 +15,27 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-	res.header(
+	// Website you wish to allow to connect
+	res.setHeader(
 		"Access-Control-Allow-Origin",
-		"https://events-galore-frontend.vercel.app"
+		"https://events-galore-frontend.vercel.app/"
 	);
+
+	// Request methods you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+
+	// Request headers you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With,content-type"
+	);
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader("Access-Control-Allow-Credentials", true);
 	next();
 });
 
